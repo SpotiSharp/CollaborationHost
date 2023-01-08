377bde5afd8322300fb09c96c7d2f7c851241615
Teddy <teddy@teddy.email>
2023-01-08 04:54:07 +0100

    now using ActionResult as return

--- SpotiSharpCollaborationHost/Controllers/CollaborationSessionController.cs
+++ SpotiSharpCollaborationHost/Controllers/CollaborationSessionController.cs
@@ -8,30 +8,40 @@ namespace SpotiSharpCollaborationHost.Controllers;
 [Route("[controller]")]
 public class CollaborationSessionController : ControllerBase
 {
-    public bool CreateSession(string sessionId)
+    [HttpPost("create-session")]
+    public ActionResult CreateSession(string sessionId)
     {
-        if (CheckSessionExists(out _, sessionId)) return false;
+        if (CheckSessionExists(out _, sessionId)) return BadRequest("Session already exists.");
         CollaborationSessionManager.CollaborationSessions.Add(new CollaborationSession(sessionId));
-        return true;
+        return Ok("Created session.");
+    }
+    
+    [HttpGet("get")]
+    public ActionResult Get(string sessionId)
+    {
+        if (!CheckSessionExists(out CollaborationSession? session, sessionId)) return BadRequest("Session doesn't exist.");
+        return Ok(session);
     }
 
-    public bool AddSongs(string sessionId, List<Song> songs)
+    [HttpPost("add-songs")]
+    public ActionResult AddSongs(string sessionId, [FromBody] List<Song> songs)
     {
-        if (!CheckSessionExists(out CollaborationSession? session, sessionId)) return false;
+        if (!CheckSessionExists(out CollaborationSession? session, sessionId)) return BadRequest("Session doesn't exist.");
         session.Songs.AddRange(songs);
-        return true;
+        return Ok("Added songs to session");
     }
 
-    public bool RemoveSongs(string sessionId, List<Song> songs)
+    [HttpPost("remove-songs")]
+    public ActionResult RemoveSongs(string sessionId, [FromBody] List<Song> songs)
     {
-        if (!CheckSessionExists(out CollaborationSession? session, sessionId)) return false;
+        if (!CheckSessionExists(out CollaborationSession? session, sessionId)) return BadRequest("Session doesn't exist.");
         foreach (var song in songs)
         {
             session.Songs.Remove(song);
         }
-        return true;
+        return Ok("Removed songs from session");
     }
-
+    
     private bool CheckSessionExists(out CollaborationSession? collaborationSession, string sessionId)
     {
         collaborationSession = null;
